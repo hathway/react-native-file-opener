a9e6f652e2699aa2ffc60d7aecf6ae0dafb56bb6
Mike Maxwell <mike@wearehathway.com>
2017-12-08 10:54:15 -0800

    Revert "RN 0.47 Breaking Change: No longer need to override this method."
    This reverts commit 6d94b8251624eac92d0354963cd07998c9b79e38.

--- android/src/main/java/com/fileopener/FileOpenerPackage.java
+++ android/src/main/java/com/fileopener/FileOpenerPackage.java
@@ -1,6 +1,7 @@
 package com.fileopener;
 
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
@@ -15,6 +16,11 @@ public class FileOpenerPackage implements ReactPackage {
   public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
     //Registering the module.
     return Arrays.<NativeModule>asList(new FileOpener(reactContext));
+  }
+
+  @Override
+  public List<Class<? extends JavaScriptModule>> createJSModules() {
+    return Collections.emptyList();
   }
 
   @Override
